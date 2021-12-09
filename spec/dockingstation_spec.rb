1b4c61a0ed90bdefc7623af69582464881311415
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2021-12-09 16:32:17 +0000

    chapter 12 added a test to raise error if docking station empty and not charge user

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,12 +8,30 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:dock).with(1).argument } # tests that docking station instances respond to dock method with one argument e.g. (bike)
 
   it "docks something" do
     bike = Bike.new # better to use Bike.new here rather than subject.release_bike because we are testing on
     expect(subject.dock(bike)).to eq bike
   end
+
+  it 'returns docked bikes' do
+    bike = Bike.new 
+    subject.dock(bike) 
+    expect(subject.bike).to eq bike
+  end
+
+  it "charges the user and does not raise an error" do
+    bike = Bike.new 
+    subject.dock(bike) 
+    expect { subject.charge }.to_not raise_error 
+  end
+  
+  it "Tries to charge user and raises an error" do
+    # no bikes at docking station
+    expect { subject.charge }.to raise_error("No bikes here!") 
+  end
+
 end
 
 
