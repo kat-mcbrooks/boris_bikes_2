c15082ed317df6aacf7d37c6fbd3a8904b57253f
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2021-12-09 15:08:05 +0000

    chapter 11 complete. instance variable and attrribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,19 @@
 require_relative 'bike' # need to load the file which establishes the Bike class, so we can create a new instance within the release_bike method at the docking station
 
 class DockingStation
+  attr_reader :bike
   def release_bike
-  Bike.new #create new instance of the Bike variable when we call release_bike method on dockingstation
+    Bike.new #create new instance of the Bike variable when we call release_bike method on dockingstation
   end
+  
   def dock(bike)
+    @bike = bike #use an instance variable so that we can add a specific instance of a bike e.g. "your bike"
   end
-  def bike
-  end
+  
+  # def bike # this is no longer needed as we're using attr_reader. this method returns the bike back to the user i.e. returns the bike we docked
+    # @bike
+  # end
 end
+
+
+
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,14 +4,17 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = subject.release_bike # can also use Bike.new here
     expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it "docks something" do
-    expect((subject).dock(bike)).to eq bike
+    bike = Bike.new # better to use Bike.new here rather than subject.release_bike because we are testing on
+    expect(subject.dock(bike)).to eq bike
   end
 end
+
+
 
