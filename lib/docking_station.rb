64704e1a79bd50ee7b2866a4aede22f7d80bf9f8
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2021-12-10 15:46:54 +0000

    Ch13. Raising an error when docking station full (with capacity of one bike). Guard condition. Rspec expect tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,7 @@
 require_relative 'bike' # need to load the file which establishes the Bike class, so we can create a new instance within the release_bike method at the docking station
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike # use this shorthand instead of using a "getter" method to get the value of the bike instance  i.e. 'def bike, @bike, end' 
 
   def release_bike
     raise "No bikes here!" unless @bike #guard statement to raise error if no bikes in station
@@ -9,11 +9,18 @@ class DockingStation
   end
   
   def dock(bike)
-    @bikes_available = true # so that the raise error message won't raise if we dock a bike to the station
-    @bike = bike #use an instance variable so that we can add a specific instance of a bike e.g. "your bike"
+    raise "Station full!" if @bike #raises an error if a bike has been docked
+    @bike = bike #instance variable so that dock method 'stores/remembers' the instance of bike which it has been passed e.g. "your_bike" and then returns it. links back up to the attr_reader at top
   end
-  
+
 end
+
+# release_bike
+# p station = DockingStation.new
+# p station.dock(Bike.new)
+# p station.dock(Bike.new)
+# p station.release_bike
+# p station.dock(Bike.new)
 
 
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,48 +1,41 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike } # one-liner syntax assumes the subject i.e. is_expected == expect(subject)
+
+  it { is_expected.to respond_to(:dock).with(1).argument } # tests that docking station instances respond to dock method with one argument e.g. (bike)
 
   describe '#release_bike' do
-    it 'releases a bike' do
+    it 'releases a bike' do #tests that docking station will release a bike if a bike has already been docked
       bike = Bike.new 
-      subject.dock(bike) 
+      subject.dock(bike) #subject == DockingStation.new
       expect(subject.release_bike).to eq bike   
     end
   end
 
-  describe '#release_bike' do
+  describe "#release_bike" do
    it "raises an error if no bikes available" do
-    # no bikes at docking station
+    # in this test, we do not dock a bike, as we are testing when no bikes at station
     expect { subject.release_bike }.to raise_error("No bikes here!") 
    end 
   end
-
-  it { is_expected.to respond_to(:dock).with(1).argument } # tests that docking station instances respond to dock method with one argument e.g. (bike)
-
-  it "docks something" do
-    bike = Bike.new # better to use Bike.new here rather than subject.release_bike because we are testing on
-    expect(subject.dock(bike)).to eq bike
+  
+  it "docks a bike" do
+    bike = Bike.new 
+    expect(subject.dock(bike)).to eq bike #tests that dock method on dstation will remember/store the bike instance and return the contents of its memory
   end
 
   it 'returns docked bikes' do
-    bike = Bike.new 
-    subject.dock(bike) 
-    expect(subject.bike).to eq bike
+    bike = Bike.new #create new bike
+    subject.dock(bike) #dock this bike to station
+    expect(subject.bike).to eq bike # we want to see the bike we just docked if we run bike method on a station
   end
 
-
-
-
-  # it "charges the user and does not raise an error" do
-  #   bike = Bike.new 
-  #   subject.dock(bike) 
-  #   expect { subject.charge }.to_not raise_error 
-  # end
-  
-  
-
+  describe "#dock" do
+    it 'raises error when station has reached capacity' do
+      bike = Bike.new
+      subject.dock(bike) #dock first bike
+      expect{ subject.dock (Bike.new) }.to raise_error "Station full!" #try to dock a second instance of Bike.new
+    end
+  end
 end
-
-
-
